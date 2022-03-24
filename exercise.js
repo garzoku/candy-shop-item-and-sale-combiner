9e1bf0cb8e7251151c1eb5e540534b73cf70ebc7
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-03-24 16:55:00 -0500

    complete assignment

--- exercise.js
+++ exercise.js
@@ -1,8 +1,24 @@
+function getItemById(items, itemID){
+    return items.find(element => element.id === itemID)
+}
 
+function buildTransactions(sales, items){
+        return sales.map((element) => {
+        let currentItem = items.find(e => e.id === element.itemId)
+            if(element.itemId === currentItem.id){
+                return{...element, price: currentItem.price, description: currentItem.description}
+            }
+    })   
+
+}
+
+function getTransactionsByItemDescription(transactions, itemDescription){
+    return list = transactions.filter(e => e.description === itemDescription)
+}
 
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+     getItemById,
+     buildTransactions,
+     getTransactionsByItemDescription
 }
